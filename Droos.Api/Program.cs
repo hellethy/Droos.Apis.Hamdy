282083d1f9d9ec7c5068a991c825da6cdba16c00
hellethy <h.ellethy1999@gmail.com>
2024-06-04 17:15:21 +0300

    EndPoint Fineshed Content and  Chapter

--- Droos.Api/Program.cs
+++ Droos.Api/Program.cs
@@ -1,3 +1,4 @@
+
 using Droos.Api.Mapper;
 using Droos.Api.Services;
 using Droos.Api.Services.Interfaces;
@@ -9,40 +10,46 @@ using Droos.Repositories.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IRepoBase<Content>, RepoBase<Content>>();
-builder.Services.AddScoped<IRepoBase<Chapter>, RepoBase<Chapter>>();
-builder.Services.AddScoped<IExamTemplateRepo, ExamTemplateRepo>();
-//builder.Services.AddScoped<IExamTemplateServices, ExamTemplateServices>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        //hamdy
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        
+        builder.Services.AddScoped<IRepoBase<Content>, RepoBase<Content>>();
+        builder.Services.AddScoped<IRepoBase<Chapter>, RepoBase<Chapter>>();
+        builder.Services.AddScoped<IExamTemplateRepo, ExamTemplateRepo>();
+        //builder.Services.AddScoped<IExamTemplateServices, ExamTemplateServices>();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
